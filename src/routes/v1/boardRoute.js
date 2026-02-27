ee253e03dd26fd4bdc958fc615bc090ea933344c
trgiahuy14 <trgiahuy14@gmail.com>
2026-02-28 00:04:56 +0700

    feat(validation): add board validation with Joi

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { boardValidation } from '~/validations/boardValidation';
 
 const Router = express.Router();
 
 Router.route('/')
-  .get((req, res) => {
-    res.status(StatusCodes.OK).json({ message: 'GET: APT get list boards' });
-  })
-  .post((req, res) => {
-    res.status(StatusCodes.CREATED).json({ message: 'POST: API create new board' });
-  });
+    .get((req, res) => {
+        res.status(StatusCodes.OK).json({ message: 'GET: APT get list boards' });
+    })
+    .post(boardValidation.createNew);
 
 export const boardRoute = Router;
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,3 +1,31 @@
-import Join from "join";
-import { StatusCodes } from "http-status-codes";
+import Joi from 'joi';
 
+import { StatusCodes } from 'http-status-codes';
+
+const createNew = async (req, res, next) => {
+    const schema = Joi.object({
+        title: Joi.string().required().min(3).max(50).trim().strict().messages({
+            'any.required': 'Title is required',
+            'string.empty': 'Title is not allowed to be empty',
+            'string.max': 'Title max 50 chars',
+            'string.min': 'Title min 3 chars',
+            'string.trim': 'Title must not have leading or trailing whitespace',
+        }),
+        description: Joi.string().required().min(3).max(256).trim().strict(),
+    });
+
+    try {
+        console.log(req.body);
+        await schema.validateAsync(req.body, { abortEarly: false });
+        res.status(StatusCodes.CREATED).json({ message: 'POST: API create new board' });
+    } catch (error) {
+        console.log(error);
+        res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+            errors: new Error(error).message,
+        });
+    }
+};
+
+export const boardValidation = {
+    createNew,
+};
